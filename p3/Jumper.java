6750e9fb8e28c2ad829e0b476bd93a54426bb1a6
acheng <31626339+wsc16340223@users.noreply.github.com>
2018-04-22 00:18:01 +0800

    Add files via upload

---
+++ p3/Jumper.java
@@ -0,0 +1,102 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ */
+
+
+package info.gridworld.actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Flower;
+import java.awt.Color;
+/**
+ * A <code>BoxBug</code> traces out a square "box" of a given size. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class Jumper extends Bug
+{
+
+    /**
+     * Constructs a red bug.
+     */
+    public Jumper()
+    {
+        setColor(Color.RED);
+    }
+
+    /**
+     * Constructs a bug of a given color.
+     * @param bugColor the color for this bug
+     */
+    public Jumper(Color bugColor)
+    {
+        setColor(bugColor);
+    }
+    /**
+     * Moves to the next location of the square.
+     */
+	
+    public void move()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+        {
+            return;
+        }
+        Location loc = getLocation();
+        Location next1 = loc.getAdjacentLocation(getDirection());
+        Location next2 = next1.getAdjacentLocation(getDirection());
+        if (gr.isValid(next2))
+        {
+            moveTo(next2);
+        }
+        else
+        {
+            removeSelfFromGrid();
+        }
+    }
+
+    /**
+     * Tests whether this bug can move forward into a location that is empty or
+     * contains a flower.
+     * @return true if this bug can move.
+     */
+    public boolean canMove()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location next1 = loc.getAdjacentLocation(getDirection());
+        Location next2 = next1.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next2))
+            return false;
+        Actor neighbor = gr.get(next2);
+        return (neighbor == null) || (neighbor instanceof Flower);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
+
+    public void act()
+    {
+        if (canMove())
+            move();
+        else
+            turn();
+    }
+}
