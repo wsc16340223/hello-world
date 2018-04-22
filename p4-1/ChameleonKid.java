ef11cc604ff7a4d00e4e6b9db81c2125c8e718c3
acheng <31626339+wsc16340223@users.noreply.github.com>
2018-04-22 18:51:30 +0800

    Add files via upload

---
+++ p4-1/ChameleonKid.java
@@ -0,0 +1,64 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Chris Nevison
+ * @author Barbara Cloud Wells
+ * @author Cay Horstmann
+ */
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+
+/**
+ * A <code>ChameleonCritter</code> takes on the color of neighboring actors as
+ * it moves through the grid. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class ChameleonKid extends Critter
+{
+    /**
+     * Randomly selects a neighbor and changes this critter's color to be the
+     * same as that neighbor's. If there are no neighbors, no action is taken.
+     */
+    public void processActors(ArrayList<Actor> actors)
+    {
+        int n = actors.size();
+        if (n == 0)
+        {
+            return;
+        }
+        Location loc = getLocation();
+        Location loc1 = loc.getAdjacentLocation(getDirection());
+        Location loc2 = loc.getAdjacentLocation(getDirection()-180);
+        for (int i = 0; i < n; i++)
+        {
+        	if (loc1.equals(actors.get(i).getLocation()))
+        	{
+        		setColor(actors.get(i).getColor());
+        		break;
+        	}
+        	else if (loc2.equals(actors.get(i).getLocation()))
+        	{
+        		setColor(actors.get(i).getColor());
+        		break;
+        	}
+        }
+    }
+
+}
+
+
+
